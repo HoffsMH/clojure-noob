fac829bb3af2b10822796cb3284bbfdf3e992e00
HoffsMH <hecker.matthew@gmail.com>
2018-01-17 08:51:27 -0500

    added some very important code

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -2,6 +2,9 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println (+ 1 2 3 4))
+  (println (* 1 2 3 4))
+  (println (first [1 2 3]))
+  )
