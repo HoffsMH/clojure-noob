d14cf349920ded595638b06484a55f570cf73eed
HoffsMH <hecker.matthew@gmail.com>
2018-01-17 16:13:02 -0500

    lots of comments and fiddling with things, its  alot more stateful than I imagined, I thought that all expressions in a file are either called by -main or not "run" i guess (println "hi") on the document before main is ok printing out hi even though it has nothing to do with -main

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -4,6 +4,27 @@
 
 (def tope "ok")
 
+; looks like I can just have expressions floating out in space with no connections?
+(or false nil :large_I_mean_venti :why_cant_I_just_say_large)
+
+; looks like def is fine without a definition
+(def tall)
+
+(def venti
+  (or false nil :large_I_mean_venti :why_cant_I_just_say_large))
+
+; looks like this gets run even though its completely outside of main
+; this seems like it could be a little dangerous no?
+(println "some text outside of main")
+
+; this is not nil (which is what I expected) its an unbound object ??
+; #object[clojure.lang.Var$Unbound 0x36916eb0 Unbound: #'clojure-noob.core/tall]
+; this also gets printed out
+(println tall)
+
+; predictably prints out :large_I_mean_venti
+(println venti)
+
 
 (defn taco
   []
@@ -17,3 +38,12 @@
 
 ; if I defined taco here I would get an error
 ; there is no hoisting and so function definition matters!
+; I guess nothing is below -main then in this file?
+
+
+; next is that I need to understand the data types that are close to string but not string
+; :is a keyword but in ruby its a "symbol" not realy understanding the switch
+; truth comparison is supposed to be fast. Is that the only advantage to these
+; what is the drawback
+
+; TODO: investigate formatters and linters
