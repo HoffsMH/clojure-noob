28f9f40f582d7d003ef9b6af364d94732afcbfd5
HoffsMH <hecker.matthew@gmail.com>
2018-01-17 15:56:56 -0500

    added a truthful comment

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -4,13 +4,16 @@
 
 (def tope "ok")
 
+
 (defn taco
   []
   (if (= "ok" tope)
-    "something was true" "something was not true")
-  )
+    "something was true" "something was not true"))
 
 (defn -main
   [& args]
   (println (taco)))
 
+
+; if I defined taco here I would get an error
+; there is no hoisting and so function definition matters!
